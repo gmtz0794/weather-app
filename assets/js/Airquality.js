75918cbf28281083dfc7e804f2f6b787ee67f3b0
Dunadunn <alxrodrig7@gmail.com>
2023-07-20 21:44:27 -0700

    added functionality to produce airquality for the API. There is a weather API listed in this code as well that has been commented out in case we decide to merge the files.

--- assets/js/Airquality.js
+++ assets/js/Airquality.js
@@ -1,10 +1,8 @@
 // Get references to the elements on the page
 const cityInput = document.querySelector('#city-input');
-// const weatherInfoContainer = document.querySelector('#weather-info');
 const airQualityContainer = document.querySelector('#air-quality');
 
-// API keys for weather and air quality APIs
-// const weatherApiKey = 'YOUR_WEATHER_API_KEY';
+// API key for the air quality API
 const airQualityApiKey = '633bb82208f93a5f0a8409af99aa77ce180a2c4e';
 
 // Function to fetch air quality data from the air quality API
@@ -21,8 +19,13 @@ const fetchAirQualityData = async (city) => {
 
 // Function to display air quality information on the page
 const displayAirQuality = (airQualityData) => {
-  // Update the airQualityContainer with the air quality data
-  // Add code to display AQI, pollutant levels, etc.
+  if (airQualityData && airQualityData.data) {
+    const aqi = airQualityData.data.aqi;
+    airQualityContainer.querySelector('.aqi').textContent = aqi;
+    // Add more air quality information here, e.g., pollutant levels, etc.
+  } else {
+    airQualityContainer.innerHTML = 'Air quality data not available.';
+  }
 };
 
 // Function to handle form submission
@@ -31,15 +34,7 @@ const formSubmitHandler = async (event) => {
   const city = cityInput.value.trim();
 
   if (city) {
-    // const weatherData = await fetchWeatherData(city); // Commented out weather-related code
-
     const airQualityData = await fetchAirQualityData(city);
-
-    // if (weatherData) {
-    //   displayWeatherInfo(weatherData); // Commented out weather-related code
-    // } else {
-    //   weatherInfoContainer.innerHTML = 'Weather data not available.'; // Commented out weather-related code
-    // }
 
     if (airQualityData) {
       displayAirQuality(airQualityData);
@@ -47,7 +42,6 @@ const formSubmitHandler = async (event) => {
       airQualityContainer.innerHTML = 'Air quality data not available.';
     }
   } else {
-    // weatherInfoContainer.innerHTML = 'Please enter a city name.'; // Commented out weather-related code
     airQualityContainer.innerHTML = '';
   }
 };
