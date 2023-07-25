4d4577d87cac3542c3e6d98b1d142b953a2a1067
Pixls112 <nguyen.phillip112@gmai.com>
2023-07-24 20:01:22 -0700

    added document selectors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
 let weather = {
     apiKey: "5f08a7e3ed8b4d6680d35453232107",
     grabWeather: function () {
-        fetch("https://api.weatherapi.com/v1/forecast.json?key=5f08a7e3ed8b4d6680d35453232107&q=London&days=5&aqi=no&alerts=no")
+        fetch("http://api.weatherapi.com/v1/current.json?key=5f08a7e3ed8b4d6680d35453232107&q=London&aqi=no")
             .then((response) => response.json())
-            .then((data) => console.log(data));
-    }
+            .then((data) => this.weatherDisplay(data));
+    },
 
-};
+    weatherDisplay: function(data) {
+        var { name, country } = data.location;
+        var { temp_f } = data.current;
+        console.log(name, country, temp_f,)
+        document.querySelector(".currentCity").innerHTML= name + ", " + country
+        document.querySelector(".temperature").innerHTML=temp_f + "F"
+    }
+};  
+
+
 
 
