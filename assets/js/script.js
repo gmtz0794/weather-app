d20e0f91b392c3285cb971ef0f68633ca891ac59
Pixls112 <nguyen.phillip112@gmai.com>
2023-07-24 20:34:44 -0700

    added eventlistner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 let weather = {
     apiKey: "5f08a7e3ed8b4d6680d35453232107",
-    grabWeather: function () {
-        fetch("http://api.weatherapi.com/v1/current.json?key=5f08a7e3ed8b4d6680d35453232107&q=London&aqi=no")
+    grabWeather: function (city) {
+        fetch("http://api.weatherapi.com/v1/current.json?key=5f08a7e3ed8b4d6680d35453232107&q=" + city +"&aqi=no")
             .then((response) => response.json())
             .then((data) => this.weatherDisplay(data));
     },
@@ -11,10 +11,15 @@ let weather = {
         var { temp_f } = data.current;
         console.log(name, country, temp_f,)
         document.querySelector(".currentCity").innerHTML= name + ", " + country
-        document.querySelector(".temperature").innerHTML=temp_f + "F"
+        document.querySelector(".temperature").innerHTML=temp_f + " F"
+    },
+    find: function() {
+     this.grabWeather(document.querySelector("#city-input").value)
     }
 };  
 
-
+document.querySelector(".submitBtn").addEventListener("click", function(){
+  weather.find();
+}) 
 
 
