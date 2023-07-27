1d74de4ee899c6391ea2ceddf71780cd479138a0
Pixls112 <nguyen.phillip112@gmai.com>
2023-07-26 18:52:09 -0700

    updated js.script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 let weather = {
     apiKey: "5f08a7e3ed8b4d6680d35453232107",
-    grabWeather: function (city) {
-        fetch("http://api.weatherapi.com/v1/current.json?key=5f08a7e3ed8b4d6680d35453232107&q=" + city +"&aqi=no")
+    grabWeather: function (name) {
+        fetch("http://api.weatherapi.com/v1/current.json?key=" + this.apiKey +"&q=" + name +"&aqi=no")
             .then((response) => response.json())
             .then((data) => this.weatherDisplay(data));
     },
